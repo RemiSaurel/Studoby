5116cf3a34ab501af0bfc8f43ea512a61555df9e
RemiSaurel <r.saurel5@gmail.com>
2023-11-19 17:03:35 +0100

    add: mainstructures

--- JAVA/structures/MainStructures.java
+++ JAVA/structures/MainStructures.java
@@ -1,6 +1,11 @@
 public class MainStructures {
 
     public static void main(String[] args) {
+        initPile();
+        initLinkedList();
+    }
+
+    public static void initPile() {
         MaPile pile = new Pile(10);
         pile.push(1);
         pile.push(2);
@@ -16,5 +21,37 @@ public class MainStructures {
         System.out.println(pile);
         System.out.println(pile.isEmpty());
         System.out.println(pile.size());
+    }
+
+    public static void initLinkedList() {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+        list.add(0, 0);
+        System.out.println(list);
+        list.add(4, 4);
+        System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
+        list.remove(3);
+        System.out.println(list);
+        System.out.println(list.get(0));
+        System.out.println(list.get(2));
+        list.set(0, 10);
+        System.out.println(list);
+        list.set(2, 30);
+        System.out.println(list);
+        System.out.println(list.contains(10));
+        System.out.println(list.contains(20));
+        System.out.println(list.indexOf(10));
+        System.out.println(list.indexOf(20));
+        System.out.println(list.isEmpty());
+        System.out.println(list.size());
+        list.clear();
+        System.out.println(list);
+        System.out.println(list.isEmpty());
+        System.out.println(list.size());
     }
 }
